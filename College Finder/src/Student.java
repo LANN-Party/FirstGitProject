cce9b5a571c6e5c7c73ae533006c12134de48f56
lrdahlquist <lrdahlquist@viewlincsci28.ad.csbsju.edu>
2017-02-22 14:38:47 -0600

    Here is the almost finished student file

---
+++ College Finder/src/Student.java
@@ -0,0 +1,181 @@
+	/**
+	 * FileName: Student.java
+	 */
+	
+	/**
+	 * Class dedicated to users who are Students
+	 * @author Logan Dahlquist
+	 * @version 2/21/2017
+	 *
+	 */
+	public class Student {
+		//Data fields
+			/**The Student's first name (String)*/
+		String firstName;
+		
+		//Data fields
+			/**The Student's last name (String)*/
+		String lastName;
+		
+		//Data fields
+			/**The Student's user name (String)*/
+		String userName;
+		
+		//Data fields
+			/**The Student's password (String)*/
+		String password;
+		
+		//Data fields
+			/**The Student's Type: Default U (Char)*/
+		char type;
+		
+		//Data fields
+			/**The Student's Current session status(char)*/
+		char status;
+		
+		//Data fields
+			/**The Student's Saved Schools (String[])*/
+		String[] savedSchools;
+		
+		//Data fields
+			/**The Student's logon status (Boolean)*/
+		boolean logon; 
+		
+		
+		/**
+		 * Return's the Student's first name
+		 * 
+		 * @param none
+		 */
+		public String getFirstName()
+		{
+			return null;
+		}
+		
+		/**
+		 * Return's the Student's Last name
+		 * 
+		 * @param none
+		 */
+		public String getLastName()
+		{
+			return null;
+		}
+		
+		/**
+		 * Return's the Student's User name
+		 * 
+		 * @param none
+		 */
+		public String getUserName()
+		{
+			return null;
+		}
+		
+		/**
+		 * Return's the Student's type
+		 * 
+		 * @param none
+		 */
+		public char getType()
+		{
+			return ' ';
+		}
+		
+		/**
+		 * Return's the Student's Username
+		 * 
+		 * @param none
+		 */
+		public String getUsername()
+		{
+			return null;
+		}
+		
+		/**
+		 * Return's the Student's Password
+		 * 
+		 * @param none
+		 */
+		public String getPassword()
+		{
+			return null;
+		}
+		
+		/**
+		 * Sets the Student's First Name
+		 * 
+		 * @param String newfName
+		 */
+		public void setFirstName(String newfName)
+		{
+			
+		}
+		
+		/**
+		 * Sets the Student's Last Name
+		 * 
+		 * @param String newlName
+		 */
+		public void setLastName(String newlName)
+		{
+			
+		}
+		
+		/**
+		 * Sets the Student's Password
+		 * 
+		 * @param String pass
+		 */
+		public void setPassword(String pass)
+		{
+			
+		}
+		
+		/**
+		 * Sets the Student's Type
+		 * 
+		 * @param String type
+		 */
+		public void setType(String type)
+		{
+			
+		}
+		
+		/**
+		 * Sets the Student's Stat
+		 * 
+		 * @param String stat
+		 */
+		public void setStatus(String stat)
+		{
+			
+		}
+		
+
+		/**
+		 * gets the Student's Stat
+		 * 
+		 * @param String stat
+		 */
+		public String getStatus(String stat)
+		{
+			return null;
+		}
+		
+
+		/**
+		 * Sets the Student's Stat
+		 * 
+		 * @param String stat
+		 */
+		public String[] getSavedSchools()
+		{
+			return null;
+		}
+		
+		
+		
+		
+		
+	}
